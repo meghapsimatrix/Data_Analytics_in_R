f2a7cc030c3a5afce4c69f348b76844563c18ca2
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2025-07-05 16:25:58 -0500

    slides updates

---
+++ slides/data/cdc_tt_data/explore_cdc.R
@@ -0,0 +1,9 @@
+library(tidytuesdayR)
+
+tuesdata <- tidytuesdayR::tt_load('2025-02-11')
+
+cdc_datasets <- tuesdata$cdc_datasets
+fpi_codes <- tuesdata$fpi_codes
+omb_codes <- tuesdata$omb_codes
+
+
